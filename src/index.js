b7ea3d6e1e0a652a1f9c4a738d7e3826cea73d34
Alik sexy <alik.want.you@gmail.com>
2024-08-07 23:54:06 +0300

    feat: add script

--- src/index.js
+++ src/index.js
@@ -35,10 +35,48 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
-};
+}
 
-function decode(expr) {
-    // write your solution here
+function decode(text) {
+	let textArray = []
+  let symbArray = [[]]
+  
+  for (let i = 0; i < text.length / 10; i++) {
+    textArray[i] = text.substr(i * 10, 10)
+    symbArray.push([])
+    for (let j = 0; j < 5; j++) {
+      if (i === 0) {
+        symbArray[i][j] = textArray[i].substr(j * 2, 2)
+      } else {        
+        symbArray[i][j] = textArray[i].substr(j * 2, 2)
+      }
+    }
+  }  
+  symbArray.pop()
+  
+  let resultArray = []
+  
+  for (let i = 0; i < symbArray.length; i++) {
+    for (let j = 0; j < 5; j++) {
+      if (symbArray[i][j] === '00') symbArray[i][j] = ''
+      if (symbArray[i][j] === '10') symbArray[i][j] = '.'
+      if (symbArray[i][j] === '11') symbArray[i][j] = '-'
+    }
+    
+    resultArray.push(symbArray[i].join(''))
+  }
+  
+  let resultLetters = []
+  
+  for (let i = 0; i < resultArray.length; i++) {
+    if (resultArray[i] === "**********") {
+      resultLetters.push(' ')
+    } else {
+      resultLetters.push(MORSE_TABLE[resultArray[i]])
+    }
+  }
+  
+  return resultLetters.join('')
 }
 
 module.exports = {
